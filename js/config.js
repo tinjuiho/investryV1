2afb9496cbeb4871000bcf18e6981ae043a0b0b4
rage1:Ray:rage2 <tinjuiho@umich.edu>
2020-07-09 20:17:59 -0700

    update cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_1D3q6LQok', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6oedlatl2o8agm62ei4rjp9njp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
